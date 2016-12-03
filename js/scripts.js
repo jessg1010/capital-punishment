924a373784b64e705bfb0d150f21a513d0fc874a
jessg1010 <jessicago@ufl.edu>
2016-12-02 23:50:11 -0500

    weektwo

--- js/scripts.js
+++ js/scripts.js
@@ -8,17 +8,47 @@ var map = L.map('map').setView([37.8, -96], 4);
 		id: 'mapbox.light'
 	}).addTo(map);
 
+	var info = L.control();
+
+	info.onAdd = function (map) {
+	    this._div = L.DomUtil.create('div', 'info');
+	    this.update();
+	    return this._div;
+	};
+
+	info.update = function (props) {
+	    this._div.innerHTML = '<h4>U.S. Death Row Inmate total</h4>' +  (props ?
+	        '<b>' + props.name + '</b><br />' + props.inmateTotal + ' people ' +
+					'</b><br />' + props.dateAbolished
+	        : 'Hover over a state to view its total');
+	};
+
+	info.addTo(map);
+
   function getColor(d) {
-		return d > 1000 ? '#800026' :
-				d > 500  ? '#BD0026' :
-				d > 200  ? '#E31A1C' :
-				d > 100  ? '#FC4E2A' :
-				d > 50   ? '#FD8D3C' :
-				d > 20   ? '#FEB24C' :
-				d > 10   ? '#FED976' :
-							'#FFEDA0';
+		return d >= 700 ? "#990000" :
+				d >= 300  ? "#b30000" :
+				d >= 200  ? "#d7301f" :
+				d >= 150  ? "#ef6548" :
+				d >= 100  ? "#fc8d59" :
+				d >= 50   ? "#fdbb84" :
+				d >= 10   ? "#fdd49e" :
+				d >= 1   ? "#fee8c8" :
+							"#c8cbcc";
 	}
 
+	function style(feature) {
+	    return {
+	        fillColor: getColor(feature.properties.inmateTotal),
+	        weight: 2,
+	        opacity: 1,
+	        color: 'white',
+	        dashArray: '3',
+	        fillOpacity: 0.7
+	    };
+	}
+
+	L.geoJson(inmateData, {style: style}).addTo(map);
 	function highlightFeature(e) {
     e.target.setStyle({
         weight: 5,
@@ -27,3 +57,62 @@ var map = L.map('map').setView([37.8, -96], 4);
         fillOpacity: 0.7
     });
 }
+
+function highlightFeature(e) {
+    var layer = e.target;
+
+    layer.setStyle({
+        weight: 5,
+        color: '#666',
+        dashArray: '',
+        fillOpacity: 0.7
+    });
+
+    if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+        layer.bringToFront();
+    }
+
+		info.update(layer.feature.properties);
+}
+
+function resetHighlight(e) {
+    geojson.resetStyle(e.target);
+		info.update();
+}
+
+function zoomToFeature(e) {
+    map.fitBounds(e.target.getBounds());
+}
+
+function onEachFeature(feature, layer) {
+    layer.on({
+        mouseover: highlightFeature,
+        mouseout: resetHighlight,
+        click: zoomToFeature
+    });
+}
+
+geojson = L.geoJson(inmateData, {
+    style: style,
+    onEachFeature: onEachFeature
+}).addTo(map);
+
+var legend = L.control({position: 'bottomright'});
+
+legend.onAdd = function (map) {
+
+    var div = L.DomUtil.create('div', 'info legend'),
+        totals = [1, 10, 50, 100, 150, 200, 300, 700],
+        labels = [];
+
+    // loop through our density intervals and generate a label with a colored square for each interval
+    for (var i = 0; i < totals.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + getColor(totals[i] + 1) + '"></i> ' +
+            totals[i] + (totals[i + 1] ? '&ndash;' + totals[i + 1] + '<br>' : '+');
+    }
+
+    return div;
+};
+
+legend.addTo(map);
